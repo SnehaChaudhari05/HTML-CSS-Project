2e0c223b44dbfeaea4cf27e9248baed21ac24110
SNEHA S CHAUDHARI <87692607+SnehaChaudhari05@users.noreply.github.com>
2021-10-03 23:32:10 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css">
+	<title>PHP Login Form</title>
+</head>
+<body>
+
+	<div class="container">
+		<h1>PHP LOGIN FORM</h1>
+		<form>
+			
+			<table>
+				<tr>
+					
+					<td><input type="text" name="username" placeholder="USERNAME"></td>
+
+				</tr>
+
+				<tr>
+					<td><input type="password" name="password" placeholder="PASSWORD"></td>
+				</tr>
+
+				<tr>
+					
+					<td><input type="submit" name="s" value="SUBMIT"></td>
+
+				</tr>
+			</table>
+
+		</form>
+
+	</div>
+
+
+	<div class="square">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+
+	<div class="circle">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+
+</body>
+</html>
